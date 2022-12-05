07ff9d88bab4408551146a8e9c841faa5b532d63
upsilon3 <114977688+upsilon3@users.noreply.github.com>
2022-12-05 10:45:29 +0300

    Add files via upload

---
+++ УП 6/b.cs
@@ -0,0 +1,16 @@
+﻿int[] source = { -1, 15, 16, 24, 16, -5, -7, 234};
+            int[] next = new int[source.Length];
+            for(int i = 0; i < source.Length; i++)
+            {
+                if(source[i] > 0 || source[i] == 0)
+                    next[i] = source[i];
+                else
+                    next[i] = 3;
+            }
+            Console.Write("Source: ");
+            foreach(int i in source)
+                Console.Write(i + " ");
+            Console.Write("\nNew: ");
+            foreach(int i in next)
+                Console.Write(i + " ");
+            Console.ReadKey(true);
